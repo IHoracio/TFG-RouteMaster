50bd735c1d903da7aa6f862dbae2399793a81d22
Juan Mañanes <121260905+mrs4ndman@users.noreply.github.com>
2025-11-27 11:44:55 +0100

    add: base del proyecto

---
+++ src/main/java/es/metrica/sept25/evolutiva_djlm/EvolutivaDjlmApplication.java
@@ -0,0 +1,13 @@
+package es.metrica.sept25.evolutiva_djlm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolutivaDjlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolutivaDjlmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/metrica/sept25/evolutiva_djlm/EvolutivaDjlmApplicationTests.java
@@ -0,0 +1,16 @@
+package es.metrica.sept25.evolutiva_djlm;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolutivaDjlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
