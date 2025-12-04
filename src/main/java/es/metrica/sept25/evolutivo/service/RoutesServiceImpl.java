71b7806799cd4a6a6c7ddf1fa92ce8a79be87c08
Mario <153368571+IHoracio@users.noreply.github.com>
2025-12-04 15:45:29 +0100

    RoutesServiceImpl Google API | now works with 1 or more destinations

--- src/main/java/es/metrica/sept25/evolutivo/service/RoutesServiceImpl.java
+++ src/main/java/es/metrica/sept25/evolutivo/service/RoutesServiceImpl.java
@@ -1,5 +1,9 @@
 package es.metrica.sept25.evolutivo.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,20 +16,34 @@ public class RoutesServiceImpl implements RoutesService {
 
 	private static final String API_URL = "https://maps.googleapis.com/maps/api/directions/json";
 	private static final String MODE = "driving";
+	private static final String OPTIMIZE = "optimize:true|";
 
 	@Autowired
     private RestTemplate restTemplate;
     
-    public RouteGroup getDirections(String origin, String destination, String language, String apiKey) {
-    	String url = UriComponentsBuilder
+    public RouteGroup getDirections(String origin, List<String> destinations, String language, String apiKey) {
+    	UriComponentsBuilder url = UriComponentsBuilder
     			.fromUriString(API_URL)
     		    .queryParam("origin", origin)
-    		    .queryParam("destination", destination)
+    		    .queryParam("destination", destinations.getFirst())
     		    .queryParam("mode", MODE)
     		    .queryParam("language", language)
-    		    .queryParam("key", apiKey)
-    		    .toUriString();
+    		    .queryParam("key", apiKey);
+    	
+    	if(destinations.size() > 1) {
+    		String finalDestination = destinations.getLast();
+    		url.queryParam("destination", finalDestination);
+    		
+    		List<String> intermediateDestinations = IntStream.range(1, destinations.size()-1)
+    				.mapToObj(i -> destinations.get(i))
+    				.toList();
+    		
+    		String waypointsParam = OPTIMIZE + intermediateDestinations.stream()
+    				.collect(Collectors.joining("|"));
+    		
+    		url.queryParam("waypoints", waypointsParam);
+    	}
         
-        return restTemplate.getForObject(url, RouteGroup.class);
+        return restTemplate.getForObject(url.toUriString(), RouteGroup.class);
     }
 }
