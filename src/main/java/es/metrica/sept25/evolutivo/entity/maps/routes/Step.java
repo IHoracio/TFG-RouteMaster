1f1b85314bfea4e7fa2682ea8da4b11511e44884
Mario <153368571+IHoracio@users.noreply.github.com>
2025-12-04 15:44:43 +0100

    Unused Spring anotation deleted

--- src/main/java/es/metrica/sept25/evolutivo/entity/maps/routes/Step.java
+++ src/main/java/es/metrica/sept25/evolutivo/entity/maps/routes/Step.java
@@ -1,10 +1,7 @@
 package es.metrica.sept25.evolutivo.entity.maps.routes;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Step {
 
 	@JsonProperty("start_location")
