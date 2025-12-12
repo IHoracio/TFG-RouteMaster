4baa1e500143cad6266b9d88e01b5b31b8b6a6f8
Mario <153368571+IHoracio@users.noreply.github.com>
2025-12-12 11:02:39 +0100

    map-page service

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { FooterComponent } from './core/layout/footer/footer.component';
 import { HeaderComponent } from './core/layout/header/header.component';
 import { MapPageComponent } from './features/pages/map-page/map-page.component';
 import { SearchBarComponent } from './features/pages/search-bar/search-bar.component';
+import { PruebaNoSubirComponent } from './features/pages/map-page/pruebaNoSubir';
 
 @Component({
   selector: 'app-root',
 
-  imports: [FooterComponent, HeaderComponent, SearchBarComponent ,SearchBarComponent, MapPageComponent],
+  imports: [FooterComponent, HeaderComponent, SearchBarComponent ,SearchBarComponent, MapPageComponent, PruebaNoSubirComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angular/src/app/services/map/map-communication.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Coords } from '../../features/pages/map-page/Utils/google-route.mapper';
+import { MapPageComponent } from '../../features/pages/map-page/map-page.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapCommunicationService {
+  private mapPageInstance: MapPageComponent | null = null;
+
+  registerMapPage(instance: MapPageComponent): boolean {
+    if (this.mapPageInstance && this.mapPageInstance !== instance) {
+      console.warn('Ya existe una instancia de MapPageComponent');
+      return false;
+    }
+    this.mapPageInstance = instance;
+    return true;
+  }
+
+  constructor() {}
+
+  sendRoute(coords: Coords[]): void {
+    console.log("Estoy en el serviece");
+    this.mapPageInstance?.drawRoute(coords);
+  }
+
+}
