441f00b33329789dc8ebb6589a2ddd2d96a5d52f
Mario <153368571+IHoracio@users.noreply.github.com>
2025-12-13 21:50:44 +0100

    moved dtos from (src/app/features/pages/map-page/Utils/google-route.mapper.ts) to (src/app/Dto/maps-dtos.ts)

---
+++ angular/src/app/Dto/maps-dtos.ts
@@ -0,0 +1,40 @@
+export interface RouteGroupResponse {
+  routes?: Route[];
+}
+
+export interface Route {
+  legs?: Leg[];
+}
+
+export interface Leg {
+  distance?: Distance;
+  duration?: Duration;
+  end_address?: string;
+  end_location?: Coords;
+  start_address?: string;
+  start_location?: Coords;
+  steps?: Step[];
+}
+
+export interface Step {
+  start_location?: Coords;
+}
+
+export interface Polyline {
+  points?: String;
+}
+
+export interface Coords { 
+  lat: number; 
+  lng: number; 
+}
+
+export interface Distance { 
+  text: string; 
+  value: number; 
+}
+
+export interface Duration { 
+  text: string; 
+  value: number; 
+}
