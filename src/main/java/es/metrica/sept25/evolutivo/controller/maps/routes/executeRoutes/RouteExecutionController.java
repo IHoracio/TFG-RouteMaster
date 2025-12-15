b106ebec69b46a20c0688445c5e7158cba83c668
Daniel-grs <danielgranadossolves@gmail.com>
2025-12-15 16:22:19 +0100

    tweak: movidos routeExecution y savedRoute  de API a '/api'

--- src/main/java/es/metrica/sept25/evolutivo/controller/maps/routes/executeRoutes/RouteExecutionController.java
+++ src/main/java/es/metrica/sept25/evolutivo/controller/maps/routes/executeRoutes/RouteExecutionController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@Tag(name = "Ejecutar Ruta")
+@Tag(name = "Ruta")
+@RequestMapping("/api/ruta")
 public class RouteExecutionController {
 
     @Autowired
