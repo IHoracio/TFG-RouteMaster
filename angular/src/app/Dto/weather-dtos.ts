87dea361f45773250fd0eb1070db981d2b0898eb
Mario <153368571+IHoracio@users.noreply.github.com>
2026-01-07 09:28:49 +0100

    modified dto weather

--- angular/src/app/Dto/weather-dtos.ts
+++ angular/src/app/Dto/weather-dtos.ts
@@ -1,9 +1,5 @@
-export interface WeatherRoute {
-    wheatherData: WeatherData[];
-}
-
 export interface WeatherData {
   address: string;
-  weatherDescription: Map<string, string>;
-  temperatures: Map<string, number>;
+  weatherDescription: { [key: string]: string };
+  temperatures: { [key: string]: number };
 }
