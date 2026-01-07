eb6dcdfaa8bb15c94c7a1b43dbf41d2346bd4f12
Lorenzo Carlos Boda Székely <lorenzoboda560@gmail.com>
2026-01-07 12:45:14 +0100

    LoginComponent started

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { SearchBarComponent } from './features/pages/search-bar/search-bar.component';
 import { LandingPageComponent } from './core/layout/landing-page/landing-page.component';
 import { CreateUserComponent } from './features/pages/create-user/create-user.component';
+import { LoginComponent } from './features/pages/login/login.component';
 
 export const routes: Routes = [
     {path: "", component: LandingPageComponent},
     {path: "register-user", component: CreateUserComponent},
     {path: "search", component: SearchBarComponent},
+    {path: "login", component: LoginComponent}
     /*{path: "map", component: MapPageComponent}*/
 ];
