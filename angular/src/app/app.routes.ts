094df69f5b1fff7baee5fd34cc5051ebffef8463
Mario <153368571+IHoracio@users.noreply.github.com>
2026-01-07 16:28:02 +0100

    user page layout without real functionalit

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { SearchBarComponent } from './features/pages/search-bar/search-bar.component';
-import { MapPageComponent } from './features/pages/map-page/map-page.component';
 import { LandingPageComponent } from './core/layout/landing-page/landing-page.component';
+import { UserPageComponent } from './features/pages/user-page/user-page.component';
 
 export const routes: Routes = [
     {path: "", component: LandingPageComponent},
     {path: "search", component: SearchBarComponent},
-    /*{path: "map", component: MapPageComponent}*/
+    {path: "user", component: UserPageComponent}
 ];
