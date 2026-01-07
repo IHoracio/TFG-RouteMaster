bbbb6adf693f18ca36ec941ed2cc3fd279bab194
Lorenzo Carlos Boda Székely <lorenzoboda560@gmail.com>
2026-01-07 16:37:39 +0100

    Header updated

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
 import { SearchBarComponent } from './features/pages/search-bar/search-bar.component';
-import { LandingPageComponent } from './core/layout/landing-page/landing-page.component';
 import { CreateUserComponent } from './features/pages/create-user/create-user.component';
 import { LoginComponent } from './features/pages/login/login.component';
 import { UserPageComponent } from './features/pages/user-page/user-page.component';
 
 export const routes: Routes = [
-    {path: "", component: LandingPageComponent},
+    {path: "", component: SearchBarComponent},
     {path: "register-user", component: CreateUserComponent},
-    {path: "search", component: SearchBarComponent},
     {path: "login", component: LoginComponent},
-    {path: "user", component: UserPageComponent}
+    {path: "user-info", component: UserPageComponent},
 ];
