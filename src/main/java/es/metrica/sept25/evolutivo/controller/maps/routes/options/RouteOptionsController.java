acfde778ef37b8b8c77085db430bd2d51501c4aa
Juan Mañanes <121260905+mrs4ndman@users.noreply.github.com>
2026-01-16 13:01:59 +0100

    docs: add swagger info for route options and user prefs

--- src/main/java/es/metrica/sept25/evolutivo/controller/maps/routes/options/RouteOptionsController.java
+++ src/main/java/es/metrica/sept25/evolutivo/controller/maps/routes/options/RouteOptionsController.java
@@ -10,25 +10,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 import es.metrica.sept25.evolutivo.domain.dto.user.preferences.EnumOptionDTO;
 import es.metrica.sept25.evolutivo.service.maps.routes.options.RouteOptionsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@Tag(name = "Opciones de ruta", description = "Endpoints para obtener opciones "
+		+ "de combustibles, tipos de mapa y emisiones por defecto")
 @RequestMapping("/api/route-options")
 public class RouteOptionsController {
 	@Autowired
-    private RouteOptionsService service;
-	
-	 @GetMapping("/fuels")
-	    public List<EnumOptionDTO> getFuels() {
-	        return service.getFuelTypes(LocaleContextHolder.getLocale());
-	    }
+	private RouteOptionsService service;
 
-	    @GetMapping("/map-types")
-	    public List<EnumOptionDTO> getMapTypes() {
-	        return service.getMapViewTypes(LocaleContextHolder.getLocale());
-	    }
+    @Operation(
+    		summary = "Devuelve los tipos de combustible disponibles en la API"
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Datos recuperados con éxito"),
+    })
+	@GetMapping("/fuels")
+	public List<EnumOptionDTO> getFuels() {
+		return service.getFuelTypes(LocaleContextHolder.getLocale());
+	}
 
-	    @GetMapping("/emissions")
-	    public List<EnumOptionDTO> getEmissions() {
-	        return service.getEmissionTypes(LocaleContextHolder.getLocale());
-	    }
+    @Operation(
+    		summary = "Devuelve los tipos de vista para mapas disponibles en la API"
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Datos recuperados con éxito"),
+    })
+	@GetMapping("/map-types")
+	public List<EnumOptionDTO> getMapTypes() {
+		return service.getMapViewTypes(LocaleContextHolder.getLocale());
+	}
+
+    @Operation(
+    		summary = "Devuelve los tipos de emisiones de gas disponibles en la API"
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Datos recuperados con éxito"),
+    })
+	@GetMapping("/emissions")
+	public List<EnumOptionDTO> getEmissions() {
+		return service.getEmissionTypes(LocaleContextHolder.getLocale());
+	}
 }
--- src/main/java/es/metrica/sept25/evolutivo/controller/preferences/PreferencesController.java
+++ src/main/java/es/metrica/sept25/evolutivo/controller/preferences/PreferencesController.java
@@ -10,19 +10,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 import es.metrica.sept25.evolutivo.domain.dto.user.preferences.EnumOptionDTO;
 import es.metrica.sept25.evolutivo.service.traductor.PreferencesService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/preferences")
+@Tag(name = "Preferencias", description = "Endpoints para obtener idiomas y temas por defecto")
 public class PreferencesController {
 
 	@Autowired
 	private PreferencesService preferencesService;
 
+    @Operation(
+    		summary = "Devuelve los lenguajes disponibles para la API"
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Datos recuperados con éxito"),
+    })
 	@GetMapping("/languages")
 	public List<EnumOptionDTO> getLanguages() {
 		return preferencesService.getLanguages(LocaleContextHolder.getLocale());
 	}
 
+    @Operation(
+    		summary = "Devuelve los temas disponibles en la API"
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Datos recuperados con éxito"),
+    })
 	@GetMapping("/themes")
 	public List<EnumOptionDTO> getThemes() {
 		return preferencesService.getThemes(LocaleContextHolder.getLocale());
