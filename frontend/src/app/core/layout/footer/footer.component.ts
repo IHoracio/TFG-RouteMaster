01cc65e5d6b4d60a6b3cc3ac4eb35ae691702ea6
Mario <153368571+IHoracio@users.noreply.github.com>
2026-02-14 11:44:45 +0100

    footer styles

--- frontend/src/app/core/layout/footer/footer.component.ts
+++ frontend/src/app/core/layout/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { DatePipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { TranslationService } from '../../../services/translation.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -11,4 +12,17 @@ import { TranslationService } from '../../../services/translation.service';
 export class FooterComponent {
   today: number = Date.now();
   translation = inject(TranslationService)
+  router = inject(Router);
+
+  navigateToPrivacy() {
+    this.router.navigate(['/privacy']).then(() => {
+      window.scrollTo(0, 0);
+    });
+  }
+
+  navigateToTerms() {
+    this.router.navigate(['/terms']).then(() => {
+      window.scrollTo(0, 0);
+    });
+  }
 }
