30bfb035451acedf72edddfc0cac2afc68347e5b
Mario <153368571+IHoracio@users.noreply.github.com>
2026-03-04 10:05:52 +0100

    modifyed weather dto, AEMET -> OPENWEATHERMAP

--- frontend/src/app/Dto/weather-dtos.ts
+++ frontend/src/app/Dto/weather-dtos.ts
@@ -1,5 +1,17 @@
 export interface WeatherData {
-  address: string;
-  weatherDescription: { [key: string]: string };
-  temperatures: { [key: string]: number };
+  address: string,
+  weatherDescription: { [key: string]: string },
+  temperatures: { [key: string]: number },
+  feelsLike: { [key: string]: number },
+  windSpeed: { [key: string]: number },
+  visibility: { [key: string]: number }
+  alerts: Alert[];
+}
+
+export interface Alert {
+  description: string,
+  event: string,
+  sender_name: string,
+  start: number,
+  end: number,
 }
