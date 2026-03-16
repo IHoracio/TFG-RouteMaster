036a6b57448df3ada70eb58e0e866513cb2fc302
Mario <153368571+IHoracio@users.noreply.github.com>
2026-03-16 12:57:11 +0100

    saved route services, now works like shared route, also I added the routeExecution method

--- backend/src/main/java/tfg/service/maps/routes/savedRoutes/SavedRouteService.java
+++ backend/src/main/java/tfg/service/maps/routes/savedRoutes/SavedRouteService.java
@@ -3,21 +3,22 @@ package tfg.service.maps.routes.savedRoutes;
 import java.util.List;
 import java.util.Optional;
 
-import tfg.domain.dto.maps.routes.savedRoutes.PointDTO;
+import tfg.domain.dto.maps.routes.FullRouteData;
 import tfg.domain.dto.maps.routes.savedRoutes.SavedRouteDTO;
+import tfg.domain.dto.maps.routes.savedRoutes.SavedRouteRequest;
 import tfg.entity.user.User;
-import tfg.enums.EmissionType;
 
 public interface SavedRouteService {
 
-	Optional<SavedRouteDTO> getSavedRoute(Long id);
+    Optional<SavedRouteDTO> getSavedRoute(String routeId);
 
-	void deleteRoute(Long id, User user);
+    void deleteRoute(String routeId, User user);
 
-	Optional<List<SavedRouteDTO>> getAllSavedRoutes(String email);
+    Optional<List<SavedRouteDTO>> getAllSavedRoutes(String email);
 
-	SavedRouteDTO saveRoute(String name, List<PointDTO> puntosDTO, User user, boolean optimizeWaypoints,
-			boolean optimizeRoute, String language, boolean avoidTolls);
-	
-	SavedRouteDTO renameRoute(String name, SavedRouteDTO savedRoute);
+    SavedRouteDTO saveRoute(SavedRouteRequest request, User user);
+    
+    SavedRouteDTO renameRoute(String name, SavedRouteDTO savedRoute);
+
+    Optional<FullRouteData> executeRoute(String routeId);
 }
