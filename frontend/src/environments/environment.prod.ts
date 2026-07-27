315a6f2bea1b156cb22ea5749ce21b1452cc572b
Mario <mariomartinmorilla@gmail.com>
2026-07-27 17:47:41 +0200

    AWS to Docker

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.tfg.mariomartinmorilla.es:8443',
+  apiUrl: 'https://api-routemaster.mariomartinmorilla.es:8443',
   googleMapsApiKey: '${GOOGLE_KEY}',
   styledMapId: 'b8466823b09eaf49247b2949',
   defaultMapId: 'b8466823b09eaf49be5d427f'
