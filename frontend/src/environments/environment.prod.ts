5bf05dd50822603ae1d4c715b0d87581b4321a01
Mario <mariomartinmorilla@gmail.com>
2026-09-06 14:08:11 +0200

    frontend new api key

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api-routemaster.mariomartinmorilla.es',
-  googleMapsApiKey: '${GOOGLE_KEY}',
+  googleMapsApiKey: '${GOOGLE_KEY_FRONTEND}',
   styledMapId: 'b8466823b09eaf49247b2949',
   defaultMapId: 'b8466823b09eaf49be5d427f'
 };
